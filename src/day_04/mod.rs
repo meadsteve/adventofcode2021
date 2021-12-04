551a5d5613b0a7403f410f8018a6af69d5976ffe
steve <steve@meadsteve.dev>
2021-12-04 14:11:15 +0100

    day 4 part 1

--- src/day_04/mod.rs
+++ src/day_04/mod.rs
@@ -1,11 +1,15 @@
 //use crate::helpers::DayData;
+use crate::helpers::DayData;
 use crate::AdventDay;
 
 pub struct DayFour();
 
 impl AdventDay for DayFour {
     fn run_part_one(&self) -> String {
-        todo!()
+        let data = DayData::from_file_path("./data/day04.txt");
+        let mut game = parse_input(data.lines());
+        let (last_call, winner) = game.play_and_get_winner();
+        format!("Thingy: {}", winner.unmarked_sum() * last_call)
     }
 
     fn run_part_two(&self) -> String {
@@ -13,6 +17,60 @@ impl AdventDay for DayFour {
     }
 }
 
+fn parse_input<I: IntoIterator<Item = String>>(input: I) -> GameSet {
+    let mut lines = input.into_iter();
+    let numbers_to_call: Vec<usize> = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+
+    lines.next();
+    let mut cards = Vec::new();
+    let mut card_numbers = Vec::new();
+    for line in lines {
+        if line.trim().is_empty() {
+            cards.push(BingoCard::new(card_numbers));
+            card_numbers = Vec::new();
+        } else {
+            card_numbers.push(
+                line.split(' ')
+                    .filter(|x| !x.is_empty())
+                    .map(|x| x.parse::<usize>().unwrap())
+                    .collect(),
+            )
+        }
+    }
+    if !card_numbers.is_empty() {
+        cards.push(BingoCard::new(card_numbers));
+    }
+    GameSet {
+        numbers_to_call,
+        cards,
+    }
+}
+
+struct GameSet {
+    numbers_to_call: Vec<usize>,
+    cards: Vec<BingoCard>,
+}
+
+impl GameSet {
+    fn play_and_get_winner(&mut self) -> (usize, BingoCard) {
+        for number in &self.numbers_to_call {
+            for card in &mut self.cards {
+                card.mark(*number);
+                if card.is_winner() {
+                    return (*number, card.clone());
+                }
+            }
+        }
+        panic!("We assumed there'd always be a winner")
+    }
+}
+
+#[derive(Clone)]
 struct BingoCard {
     grid: Vec<Vec<BingoSquare>>,
     size: usize,
@@ -57,8 +115,21 @@ impl BingoCard {
         }
         false
     }
+
+    fn unmarked_sum(&self) -> usize {
+        self.grid
+            .iter()
+            .flat_map(|squares| {
+                squares
+                    .iter()
+                    .filter(|square| !square.marked)
+                    .map(|square| square.number)
+            })
+            .sum()
+    }
 }
 
+#[derive(Clone)]
 struct BingoSquare {
     pub number: usize,
     pub marked: bool,
@@ -101,5 +172,32 @@ mod tests {
         card.mark(2);
         card.mark(4);
         assert!(card.is_winner());
+    }
+
+    #[test]
+    fn test_parsing() {
+        let input = r#"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+"#;
+        let game = parse_input(input.lines().map(|x| x.to_string()));
+        assert_eq!(game.numbers_to_call.len(), 27);
+        assert_eq!(game.cards.len(), 3);
     }
 }
--- tests/day_tests.rs
+++ tests/day_tests.rs
@@ -1,6 +1,7 @@
 use adventofcode2021::day_01::DayOne;
 use adventofcode2021::day_02::DayTwo;
 use adventofcode2021::day_03::DayThree;
+use adventofcode2021::day_04::DayFour;
 
 use adventofcode2021::AdventDay;
 use test_case::test_case;
@@ -8,6 +9,7 @@ use test_case::test_case;
 #[test_case(Box::new(DayOne()), Some("depth increases: 1316"), Some("triplet increases: 1344"); "day one")]
 #[test_case(Box::new(DayTwo()), Some("result: 2039912"), Some("result: 1942068080"); "day two")]
 #[test_case(Box::new(DayThree()), Some("Weird thingy: 1082324"), Some("Weird thingy: 1353024"); "day three")]
+#[test_case(Box::new(DayFour()), Some("Thingy: 23177"), None; "day four")]
 fn test_the_days(
     solution: Box<dyn AdventDay>,
     day_one_sol: Option<&str>,
